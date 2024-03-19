7b166234a84ac971443c6ec506ff4daaa5ad60d8
WillyPere21 <Willy.Pere@strathmore.edu>
2024-03-19 13:02:32 +0300

    landind update

--- src/routes/landing/Landing.js
+++ src/routes/landing/Landing.js
@@ -1,21 +1,50 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import HeroVideo from '../../assets/images/landing-images/background.jpg'
-import "./landing.css"
+import HeroVideo from '../../assets/images/landing-images/background.jpg'; // Import HeroVideo if needed
+import "./landing.css";
 import { ResetLocation } from "../../helpers/ResetLocation";
+import ImageSlider from "./ImageSlider";
+
+// Import images
+import backgroundImg from '../../assets/images/landing-images/background.jpg';
+import gridFiveImg from '../../assets/images/landing-images/gridFive.jpeg';
+import gridFourImg from '../../assets/images/landing-images/gridFour.jpeg';
+import gridOneImg from '../../assets/images/landing-images/gridOne.jpeg';
+import gridThreeImg from '../../assets/images/landing-images/gridThree.jpeg';
+import gridTwoImg from '../../assets/images/landing-images/gridTwo.jpeg';
 
 const Landing = ({ changeCategory }) => {
   useEffect(() => {
     document.title = "Glam Luxe";
   }, []);
+
+  const slides = [
+    { url: backgroundImg, title: 'background' },
+    { url: gridFiveImg, title: 'gridFive' },
+    { url: gridFourImg, title: 'gridFour' },
+    { url: gridOneImg, title: 'gridOne' },
+    { url: gridThreeImg, title: 'gridThree' },
+    { url: gridTwoImg, title: 'gridTwo' }
+  ];
+
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+    paddingTop: "50px" // Add your desired padding value here
+};
+
   return (
     <main className="landing">
       <article className="hero">
-        <h2>we stock and deliver quality products for homes, hotels and hospitals</h2>
-        <h1 className="more"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
-</svg></h1>
-<img src={HeroVideo} type="jpeg" alt="" />
+        <div>
+          <div style={containerStyles}>
+          <ImageSlider slides={slides}/>
+          </div>
+         <h1 className="more"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+   <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+ </svg></h1>
+</div>
       </article>
       <article className="grid">
         <section className="grid-one">
